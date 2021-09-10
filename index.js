9e84c1068a84f0ff3539660f0d5fa5e720b5de45
Andrii Matsiievskyi <andrii@xendit.co>
2021-09-10 09:18:51 +0300

    Added my implementation of function

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+// This is my implmentation of "fun" kata on aggregation with prioritized layers
+// This might be not the best solution, but this is what I can come up withing 30 minutes.
+// ALSO: Note that other candidates could see others forks of your kata, so they could possibly cheat here =)
+
 const aggregateProduct = (dataObjects = []) => {
-  const output = null
+  // Sorting by number of values in layer, so the layer with most values will be the first in array and so on.
+  const sortedByNumberOfLayers = dataObjects.sort(
+    (a, b) => Object.values(b).length - Object.values(a).length
+  );
 
-  return output
-}
+  // Reducing array, so that every iteration is overwriting keys with its own key
+  return sortedByNumberOfLayers.reduce(
+    (acc, currentValue) => ({ ...acc, ...currentValue }),
+    {}
+  );
+};
 
-module.exports = aggregateProduct
+module.exports = aggregateProduct;
